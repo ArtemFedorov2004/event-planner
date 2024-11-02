8c2ec7d302dfc1f23d86d68b9e74c296db9d2374
ArtemFedorov2004 <artem20.fedorov00@mail.ru>
2024-11-02 16:00:10 +0300

    Initial commit

---
+++ src/main/java/dev/artemfedorov/eventplanner/EventPlannerApplication.java
@@ -0,0 +1,13 @@
+package dev.artemfedorov.eventplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventPlannerApplication.class, args);
+    }
+
+}
