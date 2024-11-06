43a5df3af597c4585c2f741d59d38e2e919a1b1a
ArtemFedorov2004 <artem20.fedorov00@mail.ru>
2024-11-06 18:36:03 +0300

    fix: add check constraint to column name

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,7 +1,7 @@
 create table event
 (
     id     serial primary key,
-    name   varchar not null,
+    name varchar not null CHECK ( char_length(name) >= 1),
     date   timestamp        default current_timestamp,
     budget numeric not null default 0
 );
