d9b11d7bd11bb9d721b04d7e60433d14743e8d2a
ArtemFedorov2004 <artem20.fedorov00@mail.ru>
2024-11-16 12:25:27 +0300

    fix: update enum fields (status, category) in Task to store as varchar instead of int
    - Updated database schema: set `NOT NULL` and `DEFAULT` constraints for `c_status` and `c_category` columns.
    - Added `@PrePersist` method to initialize null fields before saving/updating, ensuring compliance with `NOT NULL` constraints.

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -9,12 +9,12 @@ create table t_event
 create table t_task
 (
     id         serial primary key,
-    c_name     varchar not null CHECK ( char_length(c_name) >= 1),
+    c_name     varchar                         not null CHECK ( char_length(c_name) >= 1),
     c_note     varchar,
-    c_category varchar,
+    c_category varchar default 'NOT_SPECIFIED' not null,
     c_date     timestamp,
-    c_status   varchar,
-    id_event   int     not null,
+    c_status   varchar default 'TO_DO'         not null,
+    id_event   int                             not null,
     constraint fk__t_task__t_event
         foreign key (id_event)
             references t_event (id)
